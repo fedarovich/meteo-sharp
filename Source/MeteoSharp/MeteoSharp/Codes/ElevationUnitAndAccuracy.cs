71c511c72a037593086515a86971c9ef57cdb40e
Pavel Fedarovich <p.fedarovich@gmail.com>
2019-09-20 14:28:17 +0300

    Added more properties to Section0 of Synop.

---
+++ Source/MeteoSharp/MeteoSharp/Codes/ElevationUnitAndAccuracy.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MeteoSharp.Codes
+{
+    /// <summary>
+    /// Code table 1845: Indicator for units of elevation, and confidence factor for accuracy of elevation
+    /// </summary>
+    public enum ElevationUnitAndAccuracy : byte
+    {
+        Invalid = 0,
+
+        /// <summary>
+        /// Unit: meter. Accuracy: within 3 meters.
+        /// </summary>
+        MeterExcellent = 1,
+
+        /// <summary>
+        /// Unit: meter. Accuracy: within 10 meters.
+        /// </summary>
+        MeterGood = 2,
+
+        /// <summary>
+        /// Unit: meter. Accuracy: within 20 meters.
+        /// </summary>
+        MeterFair = 3,
+        
+        /// <summary>
+        /// Unit: meter. Accuracy: more than 20 meters.
+        /// </summary>
+        MeterPoor = 4,
+
+        /// <summary>
+        /// Unit: foot. Accuracy: within 10 feet.
+        /// </summary>
+        FootExcellent = 5,
+
+        /// <summary>
+        /// Unit: foot. Accuracy: within 30 feet.
+        /// </summary>
+        FootGood = 6,
+
+        /// <summary>
+        /// Unit: foot. Accuracy: within 60 feet.
+        /// </summary>
+        FootFair = 7,
+        
+        /// <summary>
+        /// Unit: foot. Accuracy: more than 60 feet.
+        /// </summary>
+        FootPoor = 8,
+    }
+}
--- Source/MeteoSharp/MeteoSharp/Codes/Synop/Section0.cs
+++ Source/MeteoSharp/MeteoSharp/Codes/Synop/Section0.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using MeteoSharp.Core;
+using MeteoSharp.Measurements;
 using MeteoSharp.Time;
 
 namespace MeteoSharp.Codes.Synop
@@ -14,5 +16,39 @@ namespace MeteoSharp.Codes.Synop
         public WindSourceAndUnits WindSourceAndUnits { get; private set; }
 
         public FixedLandStationId? FixedLandStationId { get; private set; }
+
+        public SmallDecimal Latitude { get; private set; }
+
+        public SmallDecimal Longitude { get; private set; }
+
+        public GlobeQuadrant Quadrant { get; private set; }
+
+        public short MarsdenSquareNumber { get; private set; }
+
+        public SmallDecimal StationElevationValue { get; private set; }
+
+        public ElevationUnitAndAccuracy StationElevationUnitAndAccuracy { get; private set; }
+
+        public Length? StationElevation
+        {
+            get
+            {
+                switch (StationElevationUnitAndAccuracy)
+                {
+                    case ElevationUnitAndAccuracy.MeterExcellent:
+                    case ElevationUnitAndAccuracy.MeterGood:
+                    case ElevationUnitAndAccuracy.MeterFair:
+                    case ElevationUnitAndAccuracy.MeterPoor:
+                        return (StationElevationValue, LengthUnit.Meter);
+                    case ElevationUnitAndAccuracy.FootExcellent:
+                    case ElevationUnitAndAccuracy.FootGood:
+                    case ElevationUnitAndAccuracy.FootFair:
+                    case ElevationUnitAndAccuracy.FootPoor:
+                        return (StationElevationValue, LengthUnit.Foot);
+                    default:
+                        return null;
+                }
+            }
+        }
     }
 }
